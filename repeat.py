d7723c9e220b16d657ec6e89543524488f73f56a
vizhali <52244020+vizhali@users.noreply.github.com>
2019-07-10 07:12:23 -0700

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -1,8 +1,8 @@
-numb=int(input())
-vi1=list(map(int,input().split()[:numb]))
+numb7=int(input())
+vi1=list(map(int,input().split()[:numb7]))
 count=0
 vj=[]
-for i in range(0,numb+1):
+for i in range(0,numb7+1):
    if(vi1.count(i)>1):
       vj.append(i)
 if(len(vj)==0):
